4ab9a802120e182146facb1d7a71164862193e68
PAAR1198117 <pedrito.ar@gmail.com>
2018-04-08 12:34:17 -0500

    lab4

---
+++ Lab4/Lab4/Clases/Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Lab4.Models;
+
+namespace Lab4.Clases
+{
+    public class Data
+    {
+        private static Data instance;
+        public static Data Instance
+        {
+            get
+            {
+                if (instance == null) instance = new Data();
+                return instance;
+            }
+        }
+
+        public List<Estampitas1> Pelicula;
+
+        public Data()
+        {
+            Pelicula = new List<Estampitas1>();
+        }
+    }
+}
---
+++ Lab4/Lab4/Models/Estampitas 1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Globalization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Lab4.Models
+{
+    public partial class Estampitas1
+    {
+        [JsonProperty("faltantes")]
+        public List<long> Faltantes { get; set; }
+
+        [JsonProperty("coleccionadas")]
+        public List<long> Coleccionadas { get; set; }
+
+        [JsonProperty("cambios")]
+        public List<long> Cambios { get; set; }
+    }
+
+    public partial class Estampitas1
+    {
+        public static List<Dictionary<string, Estampitas1>> FromJson(string json) => JsonConvert.DeserializeObject<List<Dictionary<string, Estampitas1>>>(json, Lab4.Models.Converter.Settings);
+    }
+
+    public static class Serialize
+    {
+        public static string ToJson(this List<Dictionary<string, Estampitas1>> self) => JsonConvert.SerializeObject(self, Lab4.Models.Converter.Settings);
+    }
+
+    internal class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+            Converters = {
+                new IsoDateTimeConverter { DateTimeStyles = DateTimeStyles.AssumeUniversal }
+            },
+        };
+    }
+}
