384e2201b0ba8221cd1b92cfac15699634244623
WalterOsoy <31348535+WalterOsoy@users.noreply.github.com>
2018-04-15 22:53:01 -0600

    programa funcional

---
+++ Laboratorio4/Laboratorio4/Models/Estampas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Laboratorio4.Models
+{
+    internal class Estampas
+    {
+        public int[] faltantes;
+        public int[] coleccionadas;
+        public int[] cambios;
+    }
+}
---
+++ Laboratorio4/Laboratorio4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Laboratorio4.Startup))]
+namespace Laboratorio4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
